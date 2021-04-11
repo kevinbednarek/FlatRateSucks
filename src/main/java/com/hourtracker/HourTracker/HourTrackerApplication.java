7a715259478daa47f6af509d164a72abb68e969f
kevinbednarek <60374582+kevinbednarek@users.noreply.github.com>
2021-04-10 19:33:25 -0500

    Initial commit

---
+++ src/main/java/com/hourtracker/HourTracker/HourTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hourtracker.HourTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hourtracker/HourTracker/HourTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hourtracker.HourTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
