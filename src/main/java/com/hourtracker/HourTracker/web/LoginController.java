066ce49b9d20a90a5aff3d5effee4e878cd72957
kevinbednarek <60374582+kevinbednarek@users.noreply.github.com>
2021-04-11 17:38:28 -0500

    Login page and main page created, version 2

--- src/main/java/com/hourtracker/HourTracker/web/LoginController.java
+++ src/main/java/com/hourtracker/HourTracker/web/LoginController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-    //@RequestMapping(value="/login", method= RequestMethod.GET)
+    @RequestMapping(value="/login", method= RequestMethod.GET)
     //using getmapping instead
-    @GetMapping("/login")
+    //@GetMapping("/login")
     public String login(){
         return "login";
     }
