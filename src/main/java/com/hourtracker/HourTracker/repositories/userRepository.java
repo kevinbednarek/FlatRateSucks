212f3312b50f9340a253a9ab1315c5ed30ec8b62
kevinbednarek <60374582+kevinbednarek@users.noreply.github.com>
2021-04-12 21:17:28 -0500

    Fetching usernames from the database.

--- src/main/java/com/hourtracker/HourTracker/repositories/userRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hourtracker.HourTracker.repositories;
-
-import com.hourtracker.HourTracker.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userRepository extends JpaRepository<User, Integer> {
-}
--- src/main/java/com/hourtracker/HourTracker/service/UserDetailsServiceImpl.java
+++ src/main/java/com/hourtracker/HourTracker/service/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.hourtracker.HourTracker.service;
 
+import com.hourtracker.HourTracker.domain.User;
+import com.hourtracker.HourTracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService; //do we need this?
@@ -7,9 +9,16 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException; 
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepo;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user =  userRepo.findByUsername(username);
+
+       if (user == null)
+           throw new UsernameNotFoundException("Invalid username");
         return null;
+
     }
 }
